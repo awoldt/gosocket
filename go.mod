4038cd41429072ef97e55bd87ca0aca696c4aef6
awoldt <abreckwoldt23@gmail.com>
2026-01-10 10:52:15 -0500

    stable

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module gosocket
 
 go 1.25.1
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require github.com/gorilla/websocket v1.5.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- server.go
+++ server.go
@@ -4,29 +4,84 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/gorilla/websocket"
+	"github.com/joho/godotenv"
 )
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		if r.Host == "localhost:8080" {
-			return true
-		} else {
-			return false
-		}
-	},
-}
 
 var (
 	connections []*websocket.Conn
 	mu          sync.Mutex
 )
 
+func pluralize(count int) string {
+	if count == 1 {
+		return ""
+	}
+	return "s"
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("🟨 could not read env file... using default config")
+	}
+
+	readBufferSizeStr := os.Getenv("READ_BUFFER_SIZE")
+	writeBufferSizeStr := os.Getenv("WRITE_BUFFER_SIZE")
+	readBufferSize := 1024
+	writeBufferSize := 1024
+	if readBufferSizeStr != "" {
+		i, err := strconv.Atoi(readBufferSizeStr)
+		if err == nil {
+			readBufferSize = i
+		}
+	}
+	if writeBufferSizeStr != "" {
+		i, err := strconv.Atoi(writeBufferSizeStr)
+		if err == nil {
+			writeBufferSize = i
+		}
+	}
+
+	var allowedOrigins string
+	allowedOrigins = os.Getenv("ALLOWED_ORIGINS")
+
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  readBufferSize,
+		WriteBufferSize: writeBufferSize,
+		CheckOrigin: func(r *http.Request) bool {
+			if allowedOrigins == "" {
+				// allow any origins
+				return true
+			} else {
+				// allowed origins is a comma seperated string...
+				// split into slice and user all defined
+				origins := strings.Split(allowedOrigins, ",")
+				if len(origins) == 1 {
+					if r.Host == origins[0] {
+						return true
+					} else {
+						return false
+					}
+				} else {
+					if slices.Contains(origins, r.Host) {
+						return true
+					} else {
+						return false
+					}
+				}
+			}
+		},
+	}
+
+	fmt.Printf("config\nallowed origins: %v\nreadBufferSize: %v\nwriteBufferSize: %v\n", allowedOrigins, readBufferSize, writeBufferSize)
+
 	http.HandleFunc("/socket", func(w http.ResponseWriter, r *http.Request) {
 		conn, err := upgrader.Upgrade(w, r, nil)
 		if err != nil {
@@ -41,21 +96,45 @@ func main() {
 		connections = append(connections, conn)
 		mu.Unlock()
 
+		// notify everyone about new user
+		mu.Lock()
+		userCount := len(connections)
+		for _, c := range connections {
+			c.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("New user joined! %d user%s online", userCount, pluralize(userCount))))
+		}
+		mu.Unlock()
+
 		for {
-			messageType, _, err := conn.ReadMessage()
+			messageType, message, err := conn.ReadMessage()
 			if err != nil {
 				log.Println(err)
+				// remove connection on error
+				mu.Lock()
+				for i, c := range connections {
+					if c == conn {
+						connections = append(connections[:i], connections[i+1:]...)
+						break
+					}
+				}
+				userCount := len(connections)
+				// notify remaining users
+				for _, c := range connections {
+					c.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("User left. %d user%s online", userCount, pluralize(userCount))))
+				}
+				mu.Unlock()
 				return
 			}
 
-			// send to everyone
+			// broadcast message to everyone
+			mu.Lock()
 			for _, c := range connections {
-				if err := c.WriteMessage(messageType, []byte(fmt.Sprintf("There are %v users", len(connections)))); err != nil {
+				if err := c.WriteMessage(messageType, message); err != nil {
 					log.Println(err)
-					return
 				}
 			}
+			mu.Unlock()
 		}
+
 	})
 
 	log.Println("websocket server listening on :8080")
