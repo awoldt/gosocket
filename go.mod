17d5198de6c4a8d061efe9bf8cbd5c02bf422104
awoldt <abreckwoldt23@gmail.com>
2026-01-10 13:40:21 -0500

    implemented cli

--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.25.1
 
 require github.com/gorilla/websocket v1.5.3
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v3 v3.6.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 	"sync"
 
 	"github.com/gorilla/websocket"
+	"github.com/urfave/cli/v3"
 	"gopkg.in/yaml.v3"
 )
 
@@ -31,96 +33,117 @@ var (
 )
 
 func main() {
-	initConfig()
+	_, err := initConfig()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 
-	var upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-		CheckOrigin: func(r *http.Request) bool {
-			return true
+	cmd := &cli.Command{
+		Name: "websoget",
+		Flags: []cli.Flag{&cli.StringFlag{
+			Name:     "mode",
+			Usage:    "dev or prod",
+			Required: true,
+		}, &cli.StringFlag{
+			Name:  "port",
+			Value: "8080",
+		}},
+		Action: func(ctx context.Context, cmd *cli.Command) error {
+			mode := cmd.String("mode")
+			if mode != "dev" && mode != "prod" {
+				return fmt.Errorf("not a valid mode")
+			}
+
+			fmt.Printf("running websocket server in %v mode", mode)
+
+			initConfig()
+
+			var upgrader = websocket.Upgrader{
+				ReadBufferSize:  1024,
+				WriteBufferSize: 1024,
+				CheckOrigin: func(r *http.Request) bool {
+					return true
+				},
+			}
+
+			http.HandleFunc("/socket", func(w http.ResponseWriter, r *http.Request) {
+				conn, err := upgrader.Upgrade(w, r, nil)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+
+				defer conn.Close()
+
+				// add connection
+				mu.Lock()
+				connections = append(connections, conn)
+				mu.Unlock()
+
+				// notify everyone about new user
+				mu.Lock()
+				userCount := len(connections)
+				for _, c := range connections {
+					c.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("New user joined! %d user%s online", userCount, "RAWR")))
+				}
+				mu.Unlock()
+
+				for {
+					messageType, message, err := conn.ReadMessage()
+					if err != nil {
+						log.Println(err)
+						// remove connection on error
+						mu.Lock()
+						for i, c := range connections {
+							if c == conn {
+								connections = append(connections[:i], connections[i+1:]...)
+								break
+							}
+						}
+						userCount := len(connections)
+						// notify remaining users
+						for _, c := range connections {
+							c.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("User left. %d user%s online", userCount, "rawr")))
+						}
+						mu.Unlock()
+						return
+					}
+
+					// broadcast message to everyone
+					mu.Lock()
+					for _, c := range connections {
+						if err := c.WriteMessage(messageType, message); err != nil {
+							log.Println(err)
+						}
+					}
+					mu.Unlock()
+				}
+
+			})
+
+			log.Println("websocket server listening on :8080")
+			log.Fatal(http.ListenAndServe(":8080", nil))
+			return nil
 		},
 	}
 
-	http.HandleFunc("/socket", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		defer conn.Close()
-
-		// add connection
-		mu.Lock()
-		connections = append(connections, conn)
-		mu.Unlock()
-
-		// notify everyone about new user
-		mu.Lock()
-		userCount := len(connections)
-		for _, c := range connections {
-			c.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("New user joined! %d user%s online", userCount, pluralize(userCount))))
-		}
-		mu.Unlock()
-
-		for {
-			messageType, message, err := conn.ReadMessage()
-			if err != nil {
-				log.Println(err)
-				// remove connection on error
-				mu.Lock()
-				for i, c := range connections {
-					if c == conn {
-						connections = append(connections[:i], connections[i+1:]...)
-						break
-					}
-				}
-				userCount := len(connections)
-				// notify remaining users
-				for _, c := range connections {
-					c.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("User left. %d user%s online", userCount, pluralize(userCount))))
-				}
-				mu.Unlock()
-				return
-			}
-
-			// broadcast message to everyone
-			mu.Lock()
-			for _, c := range connections {
-				if err := c.WriteMessage(messageType, message); err != nil {
-					log.Println(err)
-				}
-			}
-			mu.Unlock()
-		}
-
-	})
-
-	log.Println("websocket server listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func pluralize(count int) string {
-	if count == 1 {
-		return ""
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
 	}
-	return "s"
 }
 
-func initConfig() {
+func initConfig() (Config, error) {
 	data, err := os.ReadFile("config.yaml")
 	if err != nil {
-		fmt.Println("no config found")
+		return Config{}, fmt.Errorf("could not find config file")
 	}
-
-	fmt.Println(string(data))
 
 	var config Config
 
 	if err = yaml.Unmarshal(data, &config); err != nil {
-		fmt.Println(err.Error())
-		panic("error while reading yaml config")
+		return Config{}, fmt.Errorf("error while reading config file")
 	}
 
-	fmt.Println(config.Apps)
+	return config, nil
 }
